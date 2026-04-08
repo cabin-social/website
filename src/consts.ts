77d7f24081a29753628a2e2ec0b4ed3c9d077701
johnajackson <hello@jaj.io>
2026-04-07 20:09:26 -0400

    Add Spacer8, Spacer16, Spacer24, Spacer32 components for blog articles

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Cabin';
-export const SITE_DESCRIPTION = 'An open-source, privacy-focused social platform built on AT Protocol.';
+export const SITE_TITLE = "Cabin";
+export const SITE_DESCRIPTION =
+  "An open source, privacy-focused social platform built on AT Protocol.";
